92abbaadeb583077a248cf149d360ca5567e7c8f
wabbi-khan <kwahab789@gmail.com>
2023-06-19 11:00:18 -0700

    ok

---
+++ login/js/main.js
@@ -0,0 +1,52 @@
+const inputs = document.querySelectorAll(".input");
+
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
+});
+
+const inputIcon = document.querySelector(".eyeIcon");
+const inputPassword = document.getElementById("password");
+
+inputIcon.addEventListener("click", () => {
+  inputIcon.classList.toggle("fa-eye");
+  inputIcon.classList.toggle("fa-eye-slash");
+  inputPassword.type = inputPassword.type === "password" ? "text" : "password";
+});
+
+// reg
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
