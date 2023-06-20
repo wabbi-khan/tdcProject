2ab6e5bc27f047a92fa940170cc5b193dee2967c
wabbi-khan <kwahab789@gmail.com>
2023-06-20 09:52:54 -0700

    registration form done

--- login/js/script.js
+++ login/js/script.js
@@ -11,9 +11,73 @@ $(document).ready(function () {
     current_fs = $(this).parent();
     next_fs = $(this).parent().next();
 
+    // ? Validation =======
+    if (current === 1) {
+      var email = $("#email").val().trim();
+      var uname = $("#uname").val().trim();
+      var pwd = $("#pwd").val().trim();
+      var cpwd = $("#cpwd").val().trim();
+
+      if (email === "") {
+        alert("Please enter Email!");
+        return false;
+      } else if (uname === "") {
+        alert("Please enter Username!");
+        return false;
+      } else if (pwd === "") {
+        alert("Please enter Password!");
+        return false;
+      } else if (cpwd === "") {
+        alert("Please enter Confirm Password!");
+        return false;
+      }
+    } else if (current === 2) {
+      var fname = $("#fname").val().trim();
+      var lname = $("#lname").val().trim();
+      var gender = $("#gender").val();
+      var date = $("#date").val().trim();
+
+      if (fname === "") {
+        alert("Please enter First Name!");
+        return false;
+      } else if (lname === "") {
+        alert("Please enter Lastname!");
+        return false;
+      } else if (gender === null || gender === "") {
+        alert("Please select gender!");
+        return false;
+      } else if (date === "") {
+        alert("Please enter date!");
+        return false;
+      }
+    } else if (current === 3) {
+      var phoneno = $("#phoneno").val().trim();
+      var address = $("#address").val().trim();
+      var city = $("#city").val().trim();
+      var state = $("#state").val();
+      var zipcode = $("#zipcode").val().trim();
+
+      if (phoneno === "") {
+        alert("Please enter Phone no!");
+        return false;
+      } else if (address === "") {
+        alert("Please enter address!");
+        return false;
+      } else if (city === "") {
+        alert("Please enter city!");
+        return false;
+      } else if (state === null || state === "") {
+        alert("Please select state!");
+        return false;
+      } else if (zipcode === "") {
+        alert("Please enter zipCode!");
+        return false;
+      }
+    }
+
+    // ===
     //Add Class Active
     $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
     //show the next fieldset
     next_fs.show();
     //hide the current fieldset with style
