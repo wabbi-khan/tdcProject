c5c64336d634c8454be4e6ef8c92d6b295f5b174
wabbi-khan <kwahab789@gmail.com>
2023-06-20 07:24:49 -0700

    creating registration patient form

--- login/js/main.js
+++ login/js/main.js
@@ -25,28 +25,3 @@ inputIcon.addEventListener("click", () => {
   inputIcon.classList.toggle("fa-eye-slash");
   inputPassword.type = inputPassword.type === "password" ? "text" : "password";
 });
-
-// reg
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
